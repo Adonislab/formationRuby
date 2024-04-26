52033f6d480b38bbd0f691c039ddeb05132dba83
Adonis <nobimetanguy19@gmail.com>
2024-04-26 08:51:01 +0100

    Data Science Application

---
+++ Ruby/lib/00_journalists.rb
@@ -0,0 +1,123 @@
+# Exercice 1
+
+journalist = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# La méthode `length` pour obtenir la taille du tableau
+taille = journalist.length
+
+# Afficher la taille du tableau
+puts "La taille du tableau est : #{taille}"
+
+# Exercice 2
+# Initialiser un compteur pour le nombre d'éléments contenant à la fois des chiffres et des lettres
+nombre_elements_mixtes = 0
+
+# Itérer à travers chaque élément du tableau
+journalist.each do |element|
+  # Vérifier si l'élément contient à la fois des chiffres et des lettres
+  if element.match?(/\d/) && element.match?(/[a-zA-Z]/)
+    # Si c'est le cas, incrémenter le compteur
+    nombre_elements_mixtes += 1
+  end
+end
+
+# Afficher le nombre total d'éléments contenant à la fois des chiffres et des lettres
+puts "Le nombre total d'éléments contenant à la fois des chiffres et des lettres dans le tableau est : #{nombre_elements_mixtes}"
+
+# Exercice 3
+
+# Convertir le prénom "aude" en une expression régulière insensible à la casse
+regex_aude = /aude/i
+
+# Initialiser un compteur pour le nombre d'éléments contenant "aude" à la suite
+nombre_elements_aude = 0
+
+# Itérer à travers chaque élément du tableau
+journalist.each do |element|
+  # Vérifier si l'élément contient "aude" à la suite
+  if element.match?(regex_aude)
+    # Si c'est le cas, incrémenter le compteur
+    nombre_elements_aude += 1
+  end
+end
+
+# Afficher le nombre total d'éléments contenant "aude" à la suite
+puts "Le nombre total d'éléments contenant les lettres 'aude' à la suite dans le tableau est : #{nombre_elements_aude}"
+
+# Exercice 4
+
+# Initialiser un compteur pour le nombre d'éléments commençant par une majuscule après le @
+nombre_elements_majuscule = 0
+
+# Itérer à travers chaque élément du tableau
+journalist.each do |element|
+  # Vérifier si l'élément commence par un @ suivi d'une majuscule
+  if element.match?(/@([A-Z])/)
+    # Si c'est le cas, incrémenter le compteur
+    nombre_elements_majuscule += 1
+  end
+end
+
+# Afficher le nombre total d'éléments commençant par une majuscule après le @
+puts "Le nombre total d'éléments commençant par une majuscule après le @ dans le tableau est : #{nombre_elements_majuscule}"
+
+# Exercice 5
+
+# Initialiser un compteur pour le nombre d'éléments contenant au moins une majuscule
+nombre_elements_majuscule = 0
+
+# Itérer à travers chaque élément du tableau
+journalist.each do |element|
+  # Vérifier si l'élément contient au moins une lettre majuscule
+  if element.match?(/[A-Z]/)
+    # Si c'est le cas, incrémenter le compteur
+    nombre_elements_majuscule += 1
+  end
+end
+
+# Afficher le nombre total d'éléments contenant au moins une majuscule
+puts "Le nombre total d'éléments contenant au moins une majuscule dans le tableau est : #{nombre_elements_majuscule}"
+
+# Exercice 6
+# Initialiser un compteur pour le nombre total de caractères underscore _
+nombre_underscore = 0
+
+# Itérer à travers chaque pseudonyme du tableau
+journalist.each do |pseudo|
+  # Utiliser la méthode `count` pour compter le nombre de underscores dans chaque pseudonyme
+  nombre_underscore += pseudo.count("_")
+end
+
+# Afficher le nombre total de caractères underscore _ dans tous les pseudonymes
+puts "Le nombre total de caractères underscore _ dans tous les pseudonymes est : #{nombre_underscore}"
+
+# Exercice 7
+# Trier le tableau par ordre alphabétique
+handles_tries = journalist.sort
+
+# Afficher les handles triés
+puts "Handles triés par ordre alphabétique :"
+handles_tries.each do |handle|
+  puts handle
+end
+
+# Exercice 7
+# Trier les handles par longueur (du plus court au plus long) et sélectionner les 50 premiers
+handles_courts = journalist.sort_by { |handle| handle.length }.first(50)
+
+# Afficher les 50 handles les plus courts
+puts "Les 50 handles les plus courts :"
+handles_courts.each do |handle|
+  puts handle
+end
+
+# Exercice 8
+# Trouver la position de "@epenser" dans le tableau
+position = journalist.index("@epenser")
+
+# Afficher la position de "@epenser"
+if position
+  puts "La position de @epenser dans le tableau est : #{position + 1}" # Ajouter 1 car les indices commencent à 0
+else
+  puts "@epenser n'a pas été trouvé dans le tableau."
+end
